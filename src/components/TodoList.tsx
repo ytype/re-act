6108cd28bcf3434890ddeca7851840187a420cad
ytype <ytype33@gmail.com>
2020-09-06 23:25:54 +0900

    cln: clean code

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -25,65 +25,6 @@ class TodoList extends React.Component<Props, State> {
     todoItems: [],
   }
 
-  getRawUrl = async (gistId: string) => {
-    const url = "https://api.github.com/gists"
-    let raw_url = ""
-    await axios.get(`${url}/${gistId}`).then((response) => {
-      const files: any = response.data.files
-      const fileName: string = Object.keys(files)[0]
-      raw_url = files[fileName].raw_url
-    })
-    return raw_url
-  }
-
-  getGist = async () => {
-    const gistId = "8759142b800011e43fd7c9a9d2c78682"
-    const raw_url = await this.getRawUrl(gistId)
-    let data = ""
-    await axios.get(raw_url).then((response) => {
-      data = response.data
-    })
-    return data
-  }
-
-  updateGist = async () => {
-    const url = "https://api.github.com"
-    const gistId = "8759142b800011e43fd7c9a9d2c78682"
-    const token: string = "efe4ab9dd87fc2264149539b06129be064661bfa"
-
-    const data = JSON.stringify({
-      description: "Hello World Examples",
-      files: {
-        "hello_world_ruby.txt": {
-          content:
-            "Run `ruby hello_world.rb` or `python hello_world.py` to print Hello World",
-          filename: "hello_world.md",
-        },
-        "hello_world_python.txt": null,
-        "new_file.txt": {
-          content: "This is a new placeholder file.",
-        },
-      },
-    })
-
-    /*
-    const star = await axios.put(`${url}/gists/${gistId}/star`, {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-    })
-    console.log(star)
-*/
-
-    const res = await axios.patch(`${url}/gists/${gistId}`, data, {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-      data: data,
-    })
-    console.log(res)
-  }
-
   onToggle = (id: number): void => {
     const { todoItems } = this.state
     const nextTodoItems: TodoItemState[] = todoItems.map((item) => {
@@ -130,17 +71,9 @@ class TodoList extends React.Component<Props, State> {
     })
   }
 
-  async componentDidMount() {
-    const _content = await this.getGist()
-    this.setState({
-      content: _content,
-    })
-    await this.updateGist()
-  }
-
   render() {
     const { onSubmit, onChange, onToggle, onRemove } = this
-    const { input, todoItems, content } = this.state
+    const { input, todoItems } = this.state
     const todoItemList: React.ReactElement[] = todoItems.map((todo) => (
       <TodoItem
         key={todo.id}
@@ -154,10 +87,6 @@ class TodoList extends React.Component<Props, State> {
     return (
       <div>
         <h1>오늘 뭐하지?</h1>
-        {/* <p>{content}</p>*/}
-        <h1>myCounter</h1>
-        <button>+</button>
-        <button>-</button>
         <form onSubmit={onSubmit}>
           <input onChange={onChange} value={input} />
           <button type="submit">추가하기</button>
